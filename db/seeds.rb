b4513a28e43fb3401964757af3f3ed2e00eb1755
Arthur Vieira <arthur.apv@gmail.com>
2016-08-02 15:39:00 -0300

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # encoding: UTF-8
 
-make_names = %w(Chevrolet Citroën Fiat Ford Honda Hyundai Kia Mitsubishi Nissan Peugeot Renault Toyota Volkswagen)
+brand_names = %w(Chevrolet Citroën Fiat Ford Honda Hyundai Kia Mitsubishi Nissan Peugeot Renault Toyota Volkswagen)
 
-make_names.each do |name|
-  Make.find_or_create_by name: name
+brand_names.each do |name|
+  Brand.find_or_create_by name: name
 end
 
-puts "#{make_names.size} car makes created"
+puts "#{brand_names.size} car brands created"
